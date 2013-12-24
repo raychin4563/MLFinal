d11d1676805e0c9558233e9f5cab5065efa02034
Ray Chin <b99902016@csie.ntu.edu.tw>
2013-12-25 03:40:14 +0800

    add git ignore to prevent upload the data

--- validateScript.sh
+++ validateScript.sh
@@ -6,7 +6,6 @@ svm_type=0
 train_data='./data/'
 test_data='./data'
 
-
 for kernel_degree in '1' '2' '3' '4'
 do
     for gamma in '0.005' '0.0075' '0.01' '0.0125' '0.015' '0.02'
@@ -15,6 +14,7 @@ do
 	do
 	    commandToRun="./libsvm-3.17/svm-tran -s $svm_type -t 1 -d $kernel_degree -g $gamma -c $cost -v $v_fold $train_data"
 	    echo $commandToRun >> validation.result
+	    $commandToRun|grep 'Cross Validation Accuracy' >> validation.result
 	done
     done
 done
