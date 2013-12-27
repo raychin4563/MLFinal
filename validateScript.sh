69ed828f3b3a59d071758369603398e350775dab
RayChin <ray40702@gmail.com>
2013-12-27 16:25:43 +0800

    add validation result

--- validateScript.sh
+++ validateScript.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 v_fold=5
-#first validate for C-svm
+#validate for C-svm
 svm_type=0
 train_data='./data/normalized_train_data2'
 test_data='./data/normalized_test_data2'
 
-for kernel_degree in '1' '2' '3' '4'
+read -p "select kernel degree: " kernel_degree
+for gamma in '0.0025' '0.005' '0.0075' '0.01' '0.0125'
 do
-    for gamma in '0.005' '0.0075' '0.01' '0.0125' '0.015' '0.02'
+    for cost in '0.05' '0.075' '0.1' '0.125' '0.15' '0.175'
     do
-	for cost in '0.075' '0.1' '0.125' '0.15' '0.175' '0.2' '0.225'
-	do
-	    commandToRun="./libsvm-3.17/svm-train -s $svm_type -t 1 -d $kernel_degree -g $gamma -c $cost -v $v_fold $train_data"
-	    echo $commandToRun >> validation.result
-	    $commandToRun|grep 'Cross Validation Accuracy' >> validation.result
-	done
+	commandToRun="./libsvm-3.17/svm-train -s $svm_type -t 1 -d $kernel_degree -g $gamma -c $cost -v $v_fold $train_data"
+	echo $commandToRun >> validation.result
+	$commandToRun|grep 'Cross Validation Accuracy' >> validation.result
     done
 done
