d8537aecf357d2a109824a605dc13e7e503ee666
RayChin <ray40702@gmail.com>
2013-12-27 16:54:22 +0800

    normalized_data try1

--- validateScript.sh
+++ validateScript.sh
@@ -12,7 +12,7 @@ do
     for cost in '0.05' '0.075' '0.1' '0.125' '0.15' '0.175'
     do
 	commandToRun="./libsvm-3.17/svm-train -s $svm_type -t 1 -d $kernel_degree -g $gamma -c $cost -v $v_fold $train_data"
-	echo $commandToRun >> validation.result
-	$commandToRun|grep 'Cross Validation Accuracy' >> validation.result
+	echo $commandToRun >> "validation.result.d$kernel_degree"
+	$commandToRun|grep 'Cross Validation Accuracy' >> "validation.result.d$kernel_degree"
     done
 done
