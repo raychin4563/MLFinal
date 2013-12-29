9a8554ad33ad54023914b6e36fa2f75e4e776575
RayChin <ray40702@gmail.com>
2013-12-30 02:15:08 +0800

    svm final gaussian test

--- validateScript.sh
+++ validateScript.sh
@@ -7,9 +7,9 @@ train_data='./data/trainDataWithNormalizedDis'
 test_data='./data/testDataWithNormalizedDis'
 
 read -p "select kernel degree: " kernel_degree
-for gamma in '0.0025' '0.005' '0.0075' '0.01' '0.0125' '0.015'
+for gamma in '0.0075' '0.01' '0.0125' '0.015' '0.0175'
 do
-    for cost in '0.05' '0.075' '0.1' '0.125' '0.15' '0.175' '0.2'
+    for cost in '0.1' '0.125' '0.15' '0.175' '0.2' '0.225' '0.25'
     do
 	commandToRun="./libsvm-3.17/svm-train -s $svm_type -t 1 -d $kernel_degree -g $gamma -c $cost -v $v_fold $train_data"
 	echo $commandToRun >> "validation.result.d$kernel_degree"
